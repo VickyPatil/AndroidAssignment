eb86f5ef62a73af899a7a9f20b9db15d7d3768ab
Vikas Patil <vikas.patil@cuelogic.com>
2020-09-10 12:47:40 +0530

    Added empty Image details activity

---
+++ ImageGallery/app/src/main/java/com/example/imagegallery/view/ImageDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.imagegallery.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.imagegallery.R;
+
+public class ImageDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_details);
+    }
+}
